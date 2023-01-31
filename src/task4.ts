c29bd1fa4cbff8b0abcdb95a39fbe26d347ddb77
alman63 <mantulenko83@mail.ru>
2023-01-31 18:35:18 +0400

    думаю...

--- src/task4.ts
+++ src/task4.ts
@@ -2,18 +2,24 @@
 export const spiral = function (matrix: number[][]): number[] {
     let arrResult: number[] = [];
     while (matrix[0] != undefined) {
+        console.log(matrix);
         if (matrix[0].length != undefined) {
             for (let i: number = 0; i < matrix[0].length; i++) {
                 arrResult.push(matrix[0][i]);
             }
             matrix.shift();
         }
-        if (matrix.length != undefined) {
+        console.log(matrix);
+        if (matrix.length != undefined && matrix[0] != undefined) {
             for (let m: number = 0; m < matrix.length - 1; m++) {
                 arrResult.push(matrix[m][matrix[m].length - 1]);
                 matrix[m].length = matrix[m].length - 1;
             }
+            if (matrix[0].length === 0) {
+                matrix.shift();
+            }
         }
+        console.log(matrix);
         if (matrix.length != undefined && matrix[0] != undefined) {
             for (let k = matrix[matrix.length - 1].length - 1; k >= 0; k--) {
                 arrResult.push(matrix[matrix.length - 1][k]);
@@ -21,13 +27,23 @@ export const spiral = function (matrix: number[][]): number[] {
                     matrix[matrix.length - 1].length - 1;
             }
             matrix.length = matrix.length - 1;
-        }
-        if (matrix.length != undefined && matrix[0] != undefined) {
-            for (let z = matrix.length; z > 0; z--) {
-                arrResult.push(matrix[z - 1][0]);
-                matrix[z - 1].shift();
+            if (matrix[0].length === 0) {
+                matrix.shift();
             }
         }
+        console.log(matrix);
+        if (matrix.length != undefined && matrix[0] != undefined) {
+            for (let z = matrix.length; z > 0; z--) {
+                if (matrix[z - 1].length != 0) {
+                    arrResult.push(matrix[z - 1][0]);
+                    matrix[z - 1].shift();
+                }
+            }
+            if (matrix[0].length === 0) {
+                matrix.shift();
+            }
+        }
+        console.log(matrix);
     }
     return arrResult;
 };
