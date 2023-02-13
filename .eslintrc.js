4b0b38bb794e288897729837b5589083ea94b2df
alman63 <mantulenko83@mail.ru>
2023-02-13 10:18:55 +0400

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,30 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
-  env: {
-    browser: true,
-    es2021: true,
-    "jest/globals": true,
-  },
-  extends: [
-    "airbnb-base",
-    "plugin:jest/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-  ],
-  overrides: [],
-  parserOptions: {
-    ecmaVersion: latest,
-    sourceType: "module",
-  },
-  rules: {
-    // "import/prefer-default-export": "off",
-    // "jest/valid-title": "off",
-    // "no-console": "off",
-    // "no-alert": "off",
-    // "no-restricted-globals": "off",
-    // "no-plusplus": "off",
-    // "import/no-unresolved": "off",
-    // "import/extensions": "off",
-  },
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    env: {
+        browser: true,
+        es2021: true,
+        'jest/globals': true,
+    },
+    extends: [
+        'airbnb-base',
+        'plugin:jest/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+    ],
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    rules: {
+        // "import/prefer-default-export": "off",
+        // "jest/valid-title": "off",
+        // "no-console": "off",
+        // "no-alert": "off",
+        // "no-restricted-globals": "off",
+        // "no-plusplus": "off",
+        // "import/no-unresolved": "off",
+        // "import/extensions": "off",
+    },
 };
