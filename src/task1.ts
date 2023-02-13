37696663961caf3bb1167a82ca9064fd9a932afc
alman63 <mantulenko83@mail.ru>
2023-02-13 20:26:06 +0400

    eslint

--- src/task1.ts
+++ src/task1.ts
@@ -1,15 +1,15 @@
 /*Функция каррирования myFunc для исходной функции суммирования от 5 аргументов*/
 const func = (a: number, b: number, c: number, d: number, e: number): number =>
-    a + b + c + d + e;
+  a + b + c + d + e;
 const myFunc = (fn: Function) => {
-    const innerFn = (N: number, args: number[]) => {
-        return (...x: number[]) => {
-            if (N <= x.length) {
-                return fn(...args, ...x);
-            }
-            return innerFn(N - x.length, [...args, ...x]);
-        };
+  const innerFn = (N: number, args: number[]) => {
+    return (...x: number[]) => {
+      if (N <= x.length) {
+        return fn(...args, ...x);
+      }
+      return innerFn(N - x.length, [...args, ...x]);
     };
-    return innerFn(fn.length, []);
+  };
+  return innerFn(fn.length, []);
 };
 export const hof = myFunc(func);
--- src/task5.ts
+++ src/task5.ts
@@ -2,23 +2,21 @@
 //результат [ "0.12.0", "1", "1.0.5", "1.2.3.4.5.6.7", "1.4.50", "1.23.45", "2.5.0" ]*/
 
 export const semverSort = function (arrString: string[]): string[] {
-    const sortFunction = function (a: string, b: string): number {
-        let k = 0;
-        while (Number(a.split('.')[k]) === Number(b.split('.')[k])) {
-            k++;
-        }
-        if (a.split('.')[k] === undefined) {
-            return -1;
-        } else {
-            if (b.split('.')[k] === undefined) {
-                return 1;
-            } else {
-                return Number(a.split('.')[k]) > Number(b.split('.')[k])
-                    ? 1
-                    : -1;
-            }
-        }
-    };
-    const arrResult = arrString.sort(sortFunction);
-    return arrResult;
+  const sortFunction = function (a: string, b: string): number {
+    let k = 0;
+    while (Number(a.split(".")[k]) === Number(b.split(".")[k])) {
+      k++;
+    }
+    if (a.split(".")[k] === undefined) {
+      return -1;
+    } else {
+      if (b.split(".")[k] === undefined) {
+        return 1;
+      } else {
+        return Number(a.split(".")[k]) > Number(b.split(".")[k]) ? 1 : -1;
+      }
+    }
+  };
+  const arrResult = arrString.sort(sortFunction);
+  return arrResult;
 };
