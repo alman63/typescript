02b890d80263695dcba313bb5fdf82493c74e675
alman63 <mantulenko83@mail.ru>
2023-03-06 10:17:09 +0400

    Update task4.ts

--- src/task4.ts
+++ src/task4.ts
@@ -1,32 +1,68 @@
 export const spiral = function (matrix: number[][]): number[] {
-  const arrResult: number[] = [];
-  const N = matrix.length;
-  let k = 0;
-  const matrixLength: number = matrix.flat().length;
-  while (matrixLength > arrResult.length) {
-    //->
-    for (let i = k; i < matrix[k].length - k; i++) {
-      arrResult.push(matrix[k][i]);
+    let arrResult: number[] = [];
+    const N = matrix.length;
+    let k = 0;
+    const matrixLength: number = matrix.flat().length;
+    function rigth(
+        k: number,
+        outputMatrix: number[],
+        inputMatrix: number[][]
+    ): number[] {
+        for (let i = k; i < inputMatrix[k].length - k; i++) {
+            outputMatrix.push(inputMatrix[k][i]);
+        }
+        return outputMatrix;
     }
-    //down
-    for (let j = k + 1; j < N - k - 1; j++) {
-      if (N - 1 - k > 1) {
-        arrResult.push(matrix[j][matrix[j].length - 1 - k]);
-      }
+    function down(
+        k: number,
+        outputMatrix: number[],
+        inputMatrix: number[][]
+    ): number[] {
+        for (let j = k + 1; j < N - k - 1; j++) {
+            if (N - 1 - k > 1) {
+                outputMatrix.push(
+                    inputMatrix[j][inputMatrix[j].length - 1 - k]
+                );
+            }
+        }
+        return outputMatrix;
     }
-    //<-
-    for (let l = matrix[N - k - 1].length - k - 1; l >= 0 + k; l--) {
-      if (N - 1 - k >= 2) {
-        arrResult.push(matrix[N - k - 1][l]);
-      }
+    function left(
+        k: number,
+        outputMatrix: number[],
+        inputMatrix: number[][]
+    ): number[] {
+        for (let l = inputMatrix[N - k - 1].length - k - 1; l >= 0 + k; l--) {
+            if (N - 1 - k >= 2) {
+                outputMatrix.push(inputMatrix[N - k - 1][l]);
+            }
+        }
+        return outputMatrix;
     }
-    //up
-    for (let m = N - k - 2; m > k; m--) {
-      if (matrix[m].length > 1) {
-        if (matrix[m][0 - k] !== undefined) arrResult.push(matrix[m][0 - k]);
-      }
+    function up(
+        k: number,
+        outputMatrix: number[],
+        inputMatrix: number[][]
+    ): number[] {
+        for (let m = N - k - 2; m > k; m--) {
+            if (inputMatrix[m].length > 1) {
+                if (inputMatrix[m][0 - k] !== undefined)
+                    outputMatrix.push(inputMatrix[m][0 - k]);
+            }
+        }
+        return outputMatrix;
     }
-    k++;
-  }
-  return arrResult;
+
+    while (matrixLength > arrResult.length) {
+        //->
+        arrResult = rigth(k, arrResult, matrix);
+        //down
+        arrResult = down(k, arrResult, matrix);
+        //<-
+        arrResult = left(k, arrResult, matrix);
+        //up
+        arrResult = up(k, arrResult, matrix);
+        k++;
+    }
+    return arrResult;
 };
